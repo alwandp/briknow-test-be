434c4cb71995488883aefe1542a856988cfa53a6
Alwan Dwi Putra <alwandp34@gmail.com>
2023-01-11 23:15:52 +0700

    feat: pagination for lessonlearned

--- app/Http/Controllers/MyLessonLearnedController.php
+++ app/Http/Controllers/MyLessonLearnedController.php
@@ -72,11 +72,11 @@ class MyLessonLearnedController extends Controller
                 })
                 ->orderBy('created_at', 'DESC')->get();
             }
-	    if (!empty($request->direktorat)){
+	       if (!empty($request->direktorat)){
                 $dir = $request->direktorat;
                 $query = Project::whereHas('divisi', function ($q) use ($dir) {
                     $q->where('user_maker', Auth::user()->personal_number);
-		    $q->where('direktorat', '=', $dir);
+		            $q->where('direktorat', '=', $dir);
                     $q->where('flag_mcs', 5);
                 })->orderBy('created_at', 'DESC')->get();
             }
@@ -116,89 +116,52 @@ class MyLessonLearnedController extends Controller
     }
 
     public function getLessonLearnedPublic(Request $request){
-        $validator = Validator::make($request->all(), [
-            'tahap' => 'nullable',
-            'divisi' => 'nullable',
-            'search' => 'nullable'
-        ]);
+        $lessonLearn = Project::with(['lesson_learned'])->join('divisis', 'projects.divisi_id', '=', 'divisis.id')->where('flag_mcs', 5)
+            ->select(DB::raw('projects.id as id, nama, slug, flag_mcs, projects.created_at, projects.updated_at, divisi_id, divisis.direktorat'))->orderBy('updated_at', 'DESC');
 
-        if (Auth::User()->role == 0) {
-             $temp = [3,4,5,6];
-            if (empty($request->tahap) && empty($request->direktorat) && empty($request->divisi) && empty($request->search)){
-                $query = Project::with(['lesson_learned'])
-                    ->where('flag_mcs', 5)
-                    ->get();
-            }
-            if (!empty($request->tahap)){
-                $tahp = $request->tahap;
-                $query = Project::whereHas('lesson_learned', function ($q) use ($tahp) {
-                    $q->where('tahap', '=', $tahp);
-                    $q->where('flag_mcs', 5);
-                })->get();
-            }
-	    if (!empty($request->direktorat)){
-                $dir = $request->direktorat;
-                $query = Project::whereHas('divisi', function ($q) use ($dir) {
-                    $q->where('direktorat', '=', $dir);
-                    $q->where('flag_mcs', 5);
-                })->get();
-            }
-            if (!empty($request->divisi)){
-                $div = $request->divisi;
-                $query = Project::whereHas('lesson_learned', function ($q) use ($div) {
-                    $q->where('divisi_id', '=', $div);
-                    $q->where('flag_mcs', 5);
-                })->get();
-            }
-            if(!empty($request->search)){
-                $key = $request->search;
-                $query = Project::whereHas('lesson_learned', function ($q) use ( $key) {
-                    $q->where('nama', 'LIKE', '%'.$key.'%');
-                    $q->where('flag_mcs', 5);
-                })->get();
-            }
-        }else{
-            if (empty($request->tahap) && empty($request->divisi) && empty($request->search)){
-                $query = Project::with(['lesson_learned'])
-                    ->where('flag_mcs', 5)
-                    ->get();
-            }
-            if (!empty($request->tahap)){
-                $tahp = $request->tahap;
-                $query = Project::whereHas('lesson_learned', function ($q) use ($tahp) {
-                    $q->where('tahap', '=', $tahp);
-                    $q->where('flag_mcs', 5);
-                })->get();
-            }
-	    if (!empty($request->direktorat)){
-                $dir = $request->direktorat;
-                $query = Project::whereHas('divisi', function ($q) use ($dir) {
-                    $q->where('direktorat', '=', $dir);
-                    $q->where('flag_mcs', 5);
-                })->get();
-            }
-            if (!empty($request->divisi)){
-                $div = $request->divisi;
-                $query = Project::whereHas('lesson_learned', function ($q) use ($div) {
-                    $q->where('divisi_id', '=', $div);
-                    $q->where('flag_mcs', 5);
-                })->get();
-            }
-            if(!empty($request->search)){
-                $key = $request->search;
-                $query = Project::whereHas('lesson_learned', function ($q) use ( $key) {
-                    $q->where('nama', 'LIKE', '%'.$key.'%');
-                    $q->where('flag_mcs', 5);
-                })->get();
+        if (!isset($request->tahap) && !isset($request->direktorat) && !isset($request->divisi) && !isset($request->search)) {
+            $query = $lessonLearn;
+        }
+        if (isset($request->tahap)){
+            $tahp = $request->tahap;
+            $query = $lessonLearn->whereHas('lesson_learned', function ($q) use ($tahp) {
+                $q->where('tahap', '=', $tahp);
+            });
+        }
+        if (isset($request->direktorat)){
+            $dir = $request->direktorat;
+            if ($dir !== "init") {
+                $query = $lessonLearn->where('direktorat', $dir);
+            } else {
+                $query = $lessonLearn->where('direktorat', 0);
             }
         }
+        if (isset($request->divisi)){
+            $div = $request->divisi;
+            if ($div !== "init") {
+                $query = $lessonLearn->where('divisi_id', $div);
+            } else {
+                $query = $lessonLearn;
+            }
+        }
+        if(isset($request->search)){
+            $key = $request->search;
+            $query = $lessonLearn->where('nama', 'LIKE', '%'.$key.'%');
+        }
+        // $query = Project::with(['lesson_learned'])->where('flag_mcs', 5);
+        $data = $query->paginate(10);
+        $count = count($data);
 
+        $paginate   = view('lesson_learned.paginate',compact('data'))->render();
+        $data['paginate'] = $paginate;
+        $data['total'] = $count;
         // $asc = Project::all()->orderBy('nama', 'ASC')->get();
+        // dd($data);
 
         return response()->json([
             "message"   => "GET Berhasil",
             "status"    => 1,
-            "data"      => $query
+            "data"      => $data
         ],200);
     }
 }
